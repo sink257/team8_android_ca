78b62e9e3681d07e7e89bf044ca283b02f3b9c50
Sankalp <sink257@hotmail.com>
2021-01-02 13:33:22 +0800

    Added a temporary button to go from main to game activity

--- app/src/main/java/eg/edu/iss/team8androidca/GameActivity.java
+++ app/src/main/java/eg/edu/iss/team8androidca/GameActivity.java
@@ -1,12 +1,12 @@
 package eg.edu.iss.team8androidca;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
